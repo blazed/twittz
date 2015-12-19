a82aa29f51a1822a29a41fee6e19ed1c18a2886c
blazed <blazed@darkstar.se>
2015-12-19 17:00:35 +0100

    Added tests

--- app/mailers/application_mailer.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@#{Rails.application.config.action_mailer.smtp_settings[:domain]}"
-  default "Message-ID" => ->(v){"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{Rails.application.config.action_mailer.smtp_settings[:domain]}>"}
-  #layout 'mailer'
-end
--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  def welcome(user)
-    @user = user
-    return unless @user
-
-    mail(to: @user.email, subject: 'test')
-  end
-end
